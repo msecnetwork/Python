c045ddd1a5f3875946f44cf98819e4e990b51cc2
msecnetwork <haoyan@godswill.com.cn>
2016-10-26 11:50:31 +0800

    Update 测试代码.py

--- 测试代码.py
+++ 测试代码.py
@@ -1,4 +1,12 @@
 #传递可变参数
-def func (* args):
-    print args
-func(1,2,3)
+def search(* t, ** d):
+    keys = d.keys()
+    values = d.values()
+    print keys
+    print values
+    for arg in t:
+        for key in keys:
+            if arg == key:
+                print "find:",d[key]
+                
+search("one","three", one = "1", two = "2" , three = "3") 
