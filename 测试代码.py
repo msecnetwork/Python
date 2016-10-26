fd8091edefd5560b215ac39ff1b9437c8901fbd2
msecnetwork <haoyan@godswill.com.cn>
2016-10-26 13:20:26 +0800

    Update 测试代码.py

--- 测试代码.py
+++ 测试代码.py
@@ -1,12 +1,9 @@
-#传递可变参数
-def search(* t, ** d):
-    keys = d.keys()
-    values = d.values()
-    print keys
-    print values
-    for arg in t:
-        for key in keys:
-            if arg == key:
-                print "find:",d[key]
-                
-search("one","three", one = "1", two = "2" , three = "3") 
+from __future__ import division
+def arithmetic(x,y,operator):
+    result = {
+        "+": x + y ,
+        "-": x - y ,
+        "*": x * y ,
+        "/": x / y
+    } 
+return result.get(operator) #返回计算结果
