83cbc8b40bf52e40c781e68f946b4ec608596f84
msecnetwork <haoyan@godswill.com.cn>
2016-10-26 11:07:25 +0800

    Update 测试代码.py

--- 测试代码.py
+++ 测试代码.py
@@ -1,6 +1,14 @@
-# -*- coding:utf-8 -*-
-from pack import *
-from pack2 import *
-
-myModule.func()
-myModule2.func2()
+#函数的默认参数
+def arithmetic(x=1,y=1,operator = "+"):
+    result = {
+        "+" : x + y ,
+        "-" : x - y ,
+        "*" : x * y ,
+        "/" : x / y
+    }
+    return result.get(operator) #返回计算结果
+print arithmetic(1,2)
+print arithmetic(1,2, "-")
+print arithmetic(y=3, operator="-")
+print arithmetic(x=4, operator="-")
+print arithmetic(x=3, y=4, operator="-")
