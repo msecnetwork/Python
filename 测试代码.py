588e42e1461c70b2f5ec6a1d739af032172e53ee
msecnetwork <haoyan@godswill.com.cn>
2016-10-26 11:28:45 +0800

    Update 测试代码.py

--- 测试代码.py
+++ 测试代码.py
@@ -1,12 +1,9 @@
-#列表作为参数传递
-def arithmetic(args = [],operator="+"):
-    x = args[0]
-    y = args[1]
-    result = {
-        "+": x + y ,
-        "-": x - y ,
-        "*": x * y ,
-        "/": x / y 
-    }
-    return result.get(operator)
-print arithmetic([1, 2])
+def append(args=[]):
+    if len(args)<=0:
+        args=[]
+    args.append(0)
+    print args
+    
+append()
+append([1])
+append()
